37a5ac260ab057e8898a48b3c3233f2bd7c895c6
Alejandro Astudillo <alejoatg@gmail.com>
2026-08-24 19:53:55 -0500

    fixes acceso email diferentes dominios

--- src/lib/branding.ts
+++ src/lib/branding.ts
@@ -3,4 +3,4 @@ export const APP_TITLE = "Sistema Informacion Forms para Utencolombia";
 export const LOGO_PATH = "/logo-uten.png";
 
 export const LOGIN_SUBTITLE =
-  "Usa tu cuenta de Google Workspace de utencolombia para acceder.";
+  "Usa tu cuenta de Google Workspace corporativa autorizada para acceder.";
